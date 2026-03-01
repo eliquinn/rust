7d9f8d423d611a1f8f05777195b887ee6a27ce7c
eliquinn <118429416+eliquinn@users.noreply.github.com>
2026-03-01 01:36:15 -0800

    a

--- test.rs
+++ test.rs
@@ -1,6 +1,30 @@
 
 
 fn main() {
+    let mut s = String::from("poop");
+    //you can make a new scope with brakcets
+    {
+        let r1 = &mut s;
+        println!("{r1}");
+    }
+    
+    //make as many mutable references as you want
+    
+    //let r1 = &s;
+   // let r2 = &s;
+    //but if there is a mutable reference, there can only be one reference
+  //  let r3 = &mut s;
+    change(&mut s); 
+    println!("{s}");
+}
+
+//mutable reference. 
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+
+fn thing() {
 
     let s1 = String::from("hello");
     //Let's pass in a reference (like adress) of s1
